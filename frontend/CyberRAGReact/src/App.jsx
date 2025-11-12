2080e5f145d7b8ddf505310d0aa402acc37d2f11
Manisha Basnet <basnetmaneesa@gmail.com>
2025-11-12 09:55:50 -0600

    CyberRAG new setup

---
+++ frontend/CyberRAGReact/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "2rem" }}>
+      <h1>Cyber RAG</h1>
+    </div>
+  );
+}
+
+export default App;
